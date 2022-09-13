c84d6ccd5976b44a5fc8166e44372d23ad5f22c1
byeonjiun <askywsa@smartscore.kr>
2022-09-13 14:58:53 +0900

    ToDoList 레이아웃 및 로직 기능 구현

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,13 +1,33 @@
 <template>
-  <div> footer </div>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearTodo">
+      clear all
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "TodoFooter"
+  name: "TodoFooter",
+  methods: {
+    clearTodo: function () {
+      localStorage.clear();
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+  }
+  .clearAllBtn {
+    color: #e20303;
+    display: block;
+  }
 </style>
